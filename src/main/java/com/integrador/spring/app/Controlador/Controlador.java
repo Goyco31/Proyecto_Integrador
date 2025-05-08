d15d78aa216a7a1d4de66c174d8271eb7bf18b28
Junior <mendozaventura14@gmail.com>
2025-05-08 13:04:04 -0500

    pequeños cambios necesarios

--- src/main/java/com/integrador/spring/app/Controlador/Controlador.java
+++ src/main/java/com/integrador/spring/app/Controlador/Controlador.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HolaControlador {
+public class Controlador {
     @GetMapping("skill")
         public String inicio() {
             return "paginaPrincipal"; // Renderiza paginaPrincipal.html
