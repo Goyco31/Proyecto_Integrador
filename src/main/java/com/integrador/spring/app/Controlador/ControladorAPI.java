8e0015a51bc0e1ec461ad0802ca3c65914d1abd4
goyco.31 <u19217844@utp.edu.pe>
2025-05-09 13:47:33 -0500

    cambio de nombre al controlador por controladorAPI

--- src/main/java/com/integrador/spring/app/Controlador/ControladorAPI.java
+++ src/main/java/com/integrador/spring/app/Controlador/ControladorAPI.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,19 +19,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/control")
 // Genera un constructor con los atributos requeridos (final), útil para inyección de dependencias
 @RequiredArgsConstructor
-public class Controlador {
+public class ControladorAPI {
     // Servicio que maneja la lógica de autenticación y registro
     private final ControladorService authService;
-    
-    //para el manejo de solicitud post a la ruta: "/""
-    @GetMapping("/")
-    public String mostrarRegistro() {
-        return "index";
-    }
-
 
     // Recibe los datos de login en el cuerpo de la solicitud y los pasa al servicio
-    @PostMapping(value = "login")
+    @PostMapping("/login")
     @ResponseBody
     public ResponseEntity<ControladorResponse> login(@RequestBody LoginRequest request) {
         // Devuelve la respuesta del servicio envuelta en un ResponseEntity con estado HTTP 200 OK
@@ -40,32 +32,11 @@ public class Controlador {
     }
 
     // Recibe los datos de registro en el cuerpo de la solicitud y los pasa al servicio(para el manejo de solicitud post a la ruta: "/control/registro")
-    @PostMapping(value = "registro")
+    @PostMapping("/registro")
     @ResponseBody
     public ResponseEntity<ControladorResponse> registro(@RequestBody RegisterRequest request) {
         // Devuelve la respuesta del servicio envuelta en un ResponseEntity con estado HTTP 200 OK
         return ResponseEntity.ok(authService.registro(request));
     }
   
-  @GetMapping("skill")
-        public String inicio() {
-            return "paginaPrincipal"; // Renderiza paginaPrincipal.html
-    }
-    
-    @GetMapping("/Explorar")
-        public String explorar() {
-            return "Explorar"; // Renderiza Explorar.html
-    }
-    
-    @GetMapping("/Clasificacion")
-        public String clasificacion() {
-            return "Clasificacion"; // Renderiza Clasificacion.html
-    }
-    
-    @GetMapping("/usuario")
-        public String usuario() {
-            return "usuario"; // Renderiza usuario.html
-    }
-  
-    
 }
