ea61db6ae6be4cbdaf0c52b1ff1f4f7999a83f94
goyco.31 <u19217844@utp.edu.pe>
2025-05-12 18:28:27 -0500

    solucion

--- src/main/java/com/integrador/spring/app/Controlador/ControladorVistas.java
+++ src/main/java/com/integrador/spring/app/Controlador/ControladorVistas.java
@@ -1,2 +1,37 @@
 package com.integrador.spring.app.Controlador;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControladorVistas {
+    @GetMapping("/")
+    public String inicio() {
+        return "paginaPrincipal";
+    }
+
+    @GetMapping("/index")
+    public String mostrarRegistro() {
+        return "index";
+    }
+
+    @GetMapping("/paginaTorneo")
+    public String paginatorneo() {
+        return "paginaTorneo";
+    }
+
+    @GetMapping("/explorar")
+    public String explorar() {
+        return "Explorar";
+    }
+    
+    @GetMapping("/clasificacion")
+    public String clasificacion() {
+        return "Clasificacion";
+    }
+    
+    @GetMapping("/usuario")
+    public String usuario() {
+        return "usuario";
+    }
+}
