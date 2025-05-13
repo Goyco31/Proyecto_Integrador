d82cec6d329f4d4ed5f96e467f4a03ec42ee7b77
goyco.31 <u19217844@utp.edu.pe>
2025-05-13 00:31:41 -0500

    cambios

--- src/main/java/com/integrador/spring/app/Controlador/ControladorVistas.java
+++ src/main/java/com/integrador/spring/app/Controlador/ControladorVistas.java
@@ -2,7 +2,7 @@ package com.integrador.spring.app.Controlador;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
