0ce1e36ae82a9ffced3d38e03cf8a490b0908790
goyco.31 <u19217844@utp.edu.pe>
2025-05-28 22:03:01 -0500

    import de controladorService en la clase controladorAPI

--- src/main/java/com/integrador/spring/app/Controlador/ControladorAPI.java
+++ src/main/java/com/integrador/spring/app/Controlador/ControladorAPI.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.integrador.spring.app.Servicio.ControladorService;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
