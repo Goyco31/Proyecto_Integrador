e415ebf07d1120a8bc0099605a0427941eb7bdf3
goyco.31 <u19217844@utp.edu.pe>
2025-06-07 12:45:10 -0500

    creacion paquete DTO

---
+++ src/main/java/com/integrador/spring/app/DTO/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.integrador.spring.app.DTO;
+
+// Importaciones de Lombok para generar automáticamente código repetitivo
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+// Anotaciones de Lombok
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    // Nombre del nuevo usuario
+    String nombre;
+    // Apellido del nuevo usuario
+    String apellido;
+    // Nombre de usuario único (nickname), usado para iniciar sesión
+    String nickname;
+    // Correo electrónico del usuario (también puede usarse como identificador o para notificaciones)
+    String correo;
+    // Contraseña del usuario, en texto plano al ser enviada
+    String contraseña;
+}
