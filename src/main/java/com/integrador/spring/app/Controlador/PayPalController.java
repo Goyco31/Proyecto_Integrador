d83eb92bdbb4aa4f1f74066ae333365fd534e8c5
Junior <mendozaventura14@gmail.com>
2025-06-10 12:28:15 -0500

    implementacion de paypal

---
+++ src/main/java/com/integrador/spring/app/Controlador/PayPalController.java
@@ -0,0 +1,34 @@
+package com.integrador.spring.app.Controlador;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.integrador.spring.app.Servicio.PayPalService;
+
+@RestController
+@RequestMapping("/api/paypal")
+public class PayPalController {
+
+    @Autowired
+    private PayPalService payPalService;
+
+    @PostMapping("/create-order")
+    public String createOrder(@RequestBody OrderRequest orderRequest) {
+        try {
+            return payPalService.createOrder(orderRequest.getCurrency(), orderRequest.getAmount());
+        } catch (Exception e) {
+            return "Error al crear orden: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/success")
+    public String success() {
+        return "Pago exitoso!";
+    }
+
+    @GetMapping("/cancel")
+    public String cancel() {
+        return "Pago cancelado.";
+    }
+}
