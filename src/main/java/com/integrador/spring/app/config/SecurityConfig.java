dabd9c702e35864cc54ae2146e8abd9033a0744c
goyco.31 <u19217844@utp.edu.pe>
2025-06-11 11:43:14 -0500

    config de rutas

--- src/main/java/com/integrador/spring/app/config/SecurityConfig.java
+++ src/main/java/com/integrador/spring/app/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                     "/clasificacion",
                     "/administrador").permitAll()
                     // Permitir acceso a las rutas de control
-                    .requestMatchers("/control/**").permitAll()
+                    .requestMatchers("/control/login", "/control/registro", "/control/refresh-token", "/control/toggle-2fa", "/control/validate-2fa").permitAll()
                     // Requiere autenticación para todo lo demás
                     .requestMatchers("/api/**").authenticated()
             )
